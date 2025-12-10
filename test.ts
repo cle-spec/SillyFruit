f651b5b6a5460c0e8a732a6d72fdcd32fc36f241
cle-spec <cle@stignatiussacschool.org>
2025-12-09 20:23:45 -0800

    Update main.blocks, test.ts

--- test.ts
+++ test.ts
@@ -1,4 +1,5 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+//Make the demo version in the test.
 let newFruit: FusionResult = null
 function fuse_a_fruit() {
     newFruit = sillyfruit.fuse(FruitKind.Apple, FruitKind.Banana)
